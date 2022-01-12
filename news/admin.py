4ef497c06f44a79191ad57eebb6e819a713f02eb
namuns <skymsn214@gmail.com>
2022-01-12 10:25:51 +0900

    news.Article 모델/마이그레이션 생성

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from news.models import Article
 
-# Register your models here.
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ["id", "title"]
+    list_display_links = ["title"]
+    search_fields = ["title"]
--- news/models.py
+++ news/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class TimestampedModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Article(TimestampedModel):
+    title = models.CharField(max_length=100, db_index=True)
+    content = models.TextField()
