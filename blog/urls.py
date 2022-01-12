946e7aeb36b6e0c95c4009edd1859127d738532c
namuns <skymsn214@gmail.com>
2022-01-12 10:13:42 +0900

    blog app 구현

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from blog.views import PostViewSet
+from blog.views import PostViewSet, post_list, post_create
 
 app_name = 'blog'
 
@@ -8,5 +8,8 @@ router = DefaultRouter()
 router.register("posts", PostViewSet)
 
 urlpatterns = [
+    path("/blog/", post_list, name="post_list"),
+    path("blog/:postId/", post_create, name="post_create"),
+    # path("blog/:postId/edit/", blog_edit, name="blog_edit"),
     path("api/", include(router.urls)),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,22 @@
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView
 from rest_framework import viewsets
+
+from blog.forms import PostForm
 from blog.serializers import PostSerializer
 from blog.models import Post
+from rest_framework import viewsets
+
+post_list = ListView.as_view(model=Post)
+
+post_create = CreateView.as_view(
+    model=Post,
+    form_class=PostForm,
+    success_url=reverse_lazy("blog:post_create"),
+)
+
+# blog_edit = UpdateView.as_view()
+
 
 
 class PostViewSet(viewsets.ModelViewSet):
